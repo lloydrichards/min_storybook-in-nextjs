f724a02fb6e7fc4eea61b78e7d072e9ce03ff533
lloydrichards <lloyd.d.richards@gmail.com>
2023-03-07 10:02:49 +0100

    fix(storybook): :bug: added redirect for storybook

--- next.config.js
+++ next.config.js
@@ -3,6 +3,15 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
+  async redirects() {
+    return [
+      {
+        source: '/storybook',
+        destination: '/storybook/index.html',
+        permanent: true,
+      },
+    ]
+  }
 }
 
 module.exports = nextConfig
