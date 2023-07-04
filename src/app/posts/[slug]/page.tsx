16b4187032739da97619021903e95ec225459e81
lloydrichards <lloyd.d.richards@gmail.com>
2023-07-04 23:16:28 +0200

    docs(contentlayer): :memo: added Story Block versions to post-02 and post-03

--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -1,15 +1,10 @@
+"use client";
 import { format, parseISO } from "date-fns";
 import { allPosts } from "../../../../.contentlayer/generated";
 import { useMDXComponent } from "next-contentlayer/hooks";
 
 export const generateStaticParams = async () =>
   allPosts.map((post) => ({ slug: post._raw.flattenedPath }));
-
-export const generateMetadata = ({ params }: { params: { slug: string } }) => {
-  const post = allPosts.find((post) => post._raw.flattenedPath === params.slug);
-  if (!post) throw new Error(`Post not found for slug: ${params.slug}`);
-  return { title: post.title };
-};
 
 const PostLayout = ({ params }: { params: { slug: string } }) => {
   const post = allPosts.find((post) => post._raw.flattenedPath === params.slug);
